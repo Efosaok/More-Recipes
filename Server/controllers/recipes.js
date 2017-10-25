e95b017853f1f37b6c0dc1633decf5e24f0baa9a
efosa <efosaokpugie@gmail.com>
2017-10-25 18:48:27 +0100

    setup controllers to handle api endpoints

--- Server/controllers/recipes.js
+++ Server/controllers/recipes.js
@@ -1,1 +1,11 @@
-recipes.js
+import db from '../db/database.json';
+
+const addRecipe = (req, res) => {
+  const idCounter = db.recipes.length + 1;
+  db.recipes.push({ id: idCounter, name: 'efosa', description: 'hweerjhreh' });
+  res.status(200).send(db);
+};
+
+export default{
+  addRecipe,
+};
