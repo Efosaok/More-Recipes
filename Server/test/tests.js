eced1d173a2e2b3e98ac2f678cd62f17891d75d9
efosa <efosaokpugie@gmail.com>
2017-10-28 21:13:24 +0100

    wrote first working test

---
+++ Server/test/tests.js
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import chai from 'chai';
+import app from '../server';
+
+const expect = chai.expect;
+
+describe('test-cases for api routes', () => {
+  describe('GET /', () => {
+    it('responds with a 200 and welcome message in json', (done) => {
+      request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, { message: 'Welcome to the beginning of nothingness.' }, done);
+    });
+  });
+
+  describe('GET /api', () => {
+    it('responds with a 200 and welcome message in json', (done) => {
+      request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, { message: 'Welcome to the beginning of nothingness.' }, done);
+    });
+  });
+});
