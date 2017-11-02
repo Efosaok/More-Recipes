ce7f5a608d7258a57b4ecd0465c57207f63a88a2
efosa <efosaokpugie@gmail.com>
2017-11-02 16:05:46 +0100

    added logic to handle sign up and sign in routes

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import userModels from '../db/models';
+import models from '../db/models';
 
 dotenv.config();
-const { Users } = userModels;
+const { Users } = models;
+const { Recipes } = models;
+const { Favorites } = models;
 
 const secret = process.env.SECRET;
-console.log(secret);
+
 class User {
   static signup(req, res) {
     const {
@@ -18,7 +20,7 @@ class User {
     } = req.body;
     bcrypt.hash(req.body.password, 10, (err, hash) => {
       if (err) {
-        return res.status(400).send({ error: 'An error occurred' });
+        return res.status(500).send({ error: `an error occured ${err}` });
       }
       const password = hash;
       return Users
@@ -42,7 +44,7 @@ class User {
           if (error.message === 'Validation error') {
             res.status(400).send({ error: 'Username or email already exists' });
           } else {
-            res.status(400).send({ error: error.message });
+            res.status(400).send({ error: `an error occured: ${error.message}` });
           }
         });
     });
@@ -57,10 +59,12 @@ class User {
       .findOne({
         where: {
           email,
-          password,
         },
       })
       .then((user) => {
+        if (!user) {
+          return res.status(400).send({ error: 'Invalid email or password' });
+        }
         bcrypt.compare(password, user.password, (err, response) => {
           if (response) {
             const payload = {
@@ -69,13 +73,28 @@ class User {
             const token = jwt.sign(payload, secret, {
               expiresIn: '10h', // expires in 1 hours
             });
-            res.status(200).send({ message: 'You have successfully logged in', token, payload });
-          } else {
-            res.status(400);
+            return res.status(200).send({ message: 'You have successfully logged in', token, payload });
           }
+          return res.status(400);
         });
       })
-      .catch(error => res.status(400).send({ error: 'Invalid Username or password' }));
+      .catch(error => res.status(500).send({ error: `an error occured: ${error.message}` }));
+  }
+
+  static getUserFavoriteRecipes(req, res) {
+    return Users.findById(req.params.userId)
+      .then((user) => {
+        user.getFavorites({
+          attributes: [],
+          include: [{
+            model: Recipes,
+          }],
+        })
+          .then((userFavourites) => {
+            res.status(200).send({ message: 'Success', userFavourites });
+          })
+          .catch(error => res.status(500).send({ error: `an error occured: ${error.message}` }));
+      });
   }
 }
 export default User;
