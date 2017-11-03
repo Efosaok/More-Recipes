5d51b1a578e4da6aff715ab373b85f2f4f6dae37
efosa <efosaokpugie@gmail.com>
2017-11-03 02:25:35 +0100

    change config.json to config.js

--- Server/db/config/config.js
+++ Server/db/config/config.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
-console.log(process.env.USERNAME)
-console.log(process.env.PASSWORD)
 module.exports = {
   development: {
-    username: 'pggcvpyn',
-    password: '_yFVWCbs1T_z9tLYys68q-lmhRoAhYm9',
-    database: 'pggcvpyn',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.USERNAME,
     host: 'babar.elephantsql.com',
     dialect: 'postgres',
     port: 5432,
